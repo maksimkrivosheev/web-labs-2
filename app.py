72d30a115e3385a472164fc3aca295b89388697d
Кривошеев Максим <m_kriv0sheev@mail.ru>
2023-12-16 12:18:49 +0700

    ispravil

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, Blueprint, render_template, request, make_response
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
-
+from lab6 import lab6
+from lab7 import lab7
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+from db.models import users
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
@@ -12,3 +17,25 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+
+app.secret_key = '123'
+user_db = "vlad_knowledge_base_orm"
+host_ip = "localhost"
+host_port = "5432"
+database_name = "knowledge_base_for_michael_orm"
+password = "123"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = "lab6.login"
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_users(user_id):
+    return users.query.get(int(user_id))
+
+app.register_blueprint(lab6)
+app.register_blueprint(lab7)
--- lab5.py
+++ lab5.py
@@ -1,211 +1,225 @@
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask import Blueprint, render_template, request, make_response, redirect, session
+from flask import Blueprint, render_template, request, redirect, session
 import psycopg2
-import os
 
-lab5 = Blueprint("lab5",__name__)
-
+lab5 = Blueprint('lab5', __name__)
 
 
 
-@lab5.route("/lab5")
-def dbConnect():
+def dBConnect():
     conn = psycopg2.connect(
-    host="127.0.0.1",
-    database="knowledge_base",
-    user="postgres",
-    port="5433"
-    )
+        host="127.0.0.1",
+        database = "knowledge_base",
+        user = "max_knowledge_base",
+        password = "123",
+        port = '5432'
+)
+    return conn;
+
+def dBClose(cursor,connection):
+    cursor.close()
+    connection.close()
+
+@lab5.route('/lab5')
+def main():
+    visibleUser = "Anon"
+    visibleUser = session.get("username")
+    conn = psycopg2.connect(
+        host="127.0.0.1",
+        database = "knowledge_base",
+        user = "julia_knowledge_base",
+        password = "123",
+        port = 5432
+)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM user;")
+
+    cur.execute("SELECT * FROM users;")
+
     result = cur.fetchall()
+
     cur.close()
     conn.close()
+
     print(result)
-    return render_template("users.html", result=result )
 
-def dbClose(cur, con):
-    cur.close()
-    con.close()
+    return render_template('lab5.html', username = visibleUser)
 
-
-@lab5.route("/lab5/users")
-def users():
+@lab5.route('/lab5/users')
+def user():
     conn = psycopg2.connect(
-    host="127.0.0.1",
-    database="knowledge_base",
-    user="postgres",
-    port="5433"
-    )
+        host="127.0.0.1",
+        database = "knowledge_base",
+        user = "julia_knowledge_base",
+        password = "123",
+        port = 5432
+)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM user;")
+    cur.execute("SELECT * FROM users;")
     result = cur.fetchall()
-    cur.close()
-    conn.close()
-    print(result)
-    return render_template("users.html", result=result )
+    return render_template('lab5users.html', users=result)
 
 
 
-
-@lab5.route('/lab5/register', methods=["GET","POST"])
+@lab5.route('/lab5/register', methods=['GET', 'POST'])
 def registerPage():
-    errors = []
-    if request.method == "GET":
-        return render_template("register.html", errors=errors)
-    
+    errors = ""
+    if request.method=='GET':
+        return render_template('register.html', errors=errors)
+    username=request.form.get('username')
 
-    username = request.form.get("username")
-    password = request.form.get("password")
-
-    if not (username or password):
-        errors.append("Пожалуйста, заполните все поля")
-        print (errors)
-        return render_template("register.html", errors=errors)
-    hashPassword=generate_password_hash(password)
+    password=request.form.get('password')
+    if not (username and password):
     
-    conn = dbConnect()
-    cur = conn.cursor()
-    
-    cur.execute(f"select username from users where username = '{username}';")
+        errors = []
+        errors = "Заполните все поля"
+        print(errors)
+        return render_template('register.html', errors=errors)
+    hashPassword = generate_password_hash(password)
+    conn=dBConnect()
+    cur=conn.cursor()
+    cur.execute(f"SELECT username From users where username= %s;", (username))
 
     if cur.fetchone() is not None:
-        errors.append("Пользователь с данным именем уже существует")
-        cur.close()
+        errors = 'the user already exists'
+        
         conn.close()
-        return render_template("register.html", errors=errors)
-    
-    cur.execute(f"insert into users (username, password) values ('{username}','{hashPassword}');")
+        cur.close()
+        return render_template('register.html', errors=errors)
 
+    cur.execute(f"INSERT INTO users (username, password) VALUES (%s, %s);", (username, hashPassword))
     conn.commit()
-    cur.close()
     conn.close()
+    cur.close()
+    return redirect('/lab5/login')
 
-    return redirect('/lab5/login5')
 
-
-
-@lab5.route("/lab5/login5", methods=['GET','POST'])
+@lab5.route('/lab5/login', methods=['GET', 'POST'])
 def loginPage():
     errors = []
-    if request.method == "GET":
-        return render_template("login5.html", errors=errors)
+    if request.method=='GET':
+        return render_template('login.html', errors=errors)
+
+    username=request.form.get('username')
+    password=request.form.get('password')
+
+    if not (username and password):
     
-    username = request.form.get("username")
-    password = request.form.get("password")
-    
-    if not (username or password):
-        errors.append("Пожалуйста, заполните все поля")
-        return render_template("login5.html", errors=errors)
+        errors = []
+        errors = "Заполните все поля"
+        print(errors)
+        return render_template('login.html', errors=errors)
 
+    conn=dBConnect()
+    cur=conn.cursor()
 
-    conn = dbConnect()
-    cur = conn.cursor()
-
-    cur.execute(f"select id, password from users where username = '{username}'")
-
-    result=cur.fetchone()
+    cur.execute(f"SELECT id, password From users where username = '{username}';")
+    result = cur.fetchone()
 
     if result is None:
-        errors.append("Неправильный логин или пароль")
-        dbClose(cur,conn)
-        return render_template("login5.html", errors=errors)
-    
+        errors = []
+        errors = 'Непавильный логин или пароль'
+        
+        dBClose(cur,conn)
+        return render_template('login.html', errors=errors)
+
     userID, hashPassword = result
+    
 
-   
     if check_password_hash(hashPassword, password):
-        session['id'] = userID
-        session['username'] = username
-        dbClose(cur, conn)
-        return redirect("/lab5/new_article")
+            session['id'] = userID
+            session['username'] = username
+            dBClose(cur,conn)
+            return redirect ("/lab5")
+
     else:
-        errors.append("Неправильный пароль или логин")
-        return render_template("login5.html", errors=errors)
-
-    
+            errors.append("Неправильный логин или пароль")
+            return render_template("login.html", errors=errors)
 
 
-    
 
-@lab5.route("/lab5/new_article")
+@lab5.route('/lab5/new_article', methods=['GET', 'POST'])
 def createArticle():
-    errors=[]
+    errors = []
     userID = session.get("id")
 
     if userID is not None:
-        if request.method =='GET':
-            return render_template("new_article.html")
+        if request.method=='GET':
+            return render_template('note.html')
+
     
-        if request.method =='POST':
+        if request.method=='POST':
+            text_article = request.form.get("text_article")
             title = request.form.get("title_article")
-            text_article= request.form.get("text_article")
+
+            if len('text_article') == 0:
+                errors.append ("Заполните поле")
+                return render_template('note.html', errors=errors)
+
+            conn = dBConnect()
+            cur = conn.cursor()
+            is_public = request.form.get('publish')
+            cur.execute(f"INSERT INTO articles(user_id, title, article_text, is_public) VALUES (%s, %s, %s, %s) RETURNING id", (userID, title, text_article, is_public))
+
         
-        if len(text_article) == 0:
-            errors=['Заполните текст']
-            return render_template('new_article.html', errors=errors)
-        
-        conn, cur = dbConnect()
+            new_article_id = cur.fetchone()[0]
+            conn.commit()
+            dBClose(cur,conn)
+            return redirect(f"/lab5/articles/{new_article_id}")
 
-        cur.execute(f"INSERT INTO articles (user_id, title, article_text) VALUES ({userID}, '{title}', '{text_article}') RETURNING id")
-        new_article_id = cur.fetchone()[0]
-        conn.commit()
+        return redirect ("/lab5/login")
 
-        dbClose(conn, cur)
-        return redirect(f"/lab5/new_article/{new_article_id}")
-    return redirect("/lab5/login5")
-
-def dbConnect():
-    try:
-        conn = psycopg2.connect(
-            database="knowledge_base_for_vlad",
-            user="postgres",
-            password="postgres",
-            host="127.0.0.1",
-            
-        )
-        cur = conn.cursor()
-        return conn, cur
-    except psycopg2.Error as e:
-        print(f"An error occurred while connecting to the database: {e}")
-        return None, None
-        
-@lab5.route("/lab5/new_article",methods=['POST'])
-def getArticle(article_id):
-    userID = session.get('id')
-
-    if userID is not None:
-        conn=dbConnect()
-        conn, cur = dbConnect()
-
-
-    cur.execute(f"SELECT title, article_text FROM articles WHERE id = {article_id} and user_id = {userID}")
-    articleBody = cur.fetchone()
-
-    dbClose(cur, conn)
-    if articleBody is None:
-        return 'Not found!'
-    
-    text= articleBody[1].splitlines()
-    
-
-    return render_template("new_article.html", article_text=text, article_title=articleBody[0], username=session.get('username'))
-
-@lab5.route("/lab5/articles")
-def getArticles():
+@lab5.route("/lab5/articles/<string:article_id>")
+def gerArticle(article_id):
     userID = session.get("id")
-    
-    if userID is not None:
-        conn, cur = dbConnect()
-        
-        cur.execute(f"SELECT title, article_text FROM articles WHERE user_id = {userID}")
-        articles = cur.fetchall()
-        
-        dbClose(conn, cur)
-        
-        return render_template("articles.html", articles=articles)
-    else:
-        return redirect("/lab5/login5")
 
-if __name__ == "__main__":
-    lab5.run(debug=True)
+    if userID is not None:
+        conn = dBConnect()
+        cur = conn.cursor()
+
+        cur.execute(f"SELECT title, article_text FROM articles WHERE id = {article_id}")
+        #  and user_id = %s
+        articleBody = cur.fetchone()
+
+        dBClose(cur, conn)
+
+        if articleBody is None:
+            return "Not found!"
+
+        text = articleBody[1].splitlines()
+
+        return render_template("articleN.html", article_text=text,
+        article_title =articleBody[0], username=session.get("username"))
+
+@lab5.route("/lab5/article_list")
+def getArticleList():
+    userID = session.get("id")
+    username = session.get("username")
+    articles_list = "Нет статей"
+    if userID is not None:
+        conn = dBConnect()
+        cur = conn.cursor()
+        
+        cur.execute(f"SELECT id, title FROM articles WHERE user_id = {userID}")
+        articles_list = cur.fetchall()
+
+    return render_template("article_list.html", articles_list=articles_list, username=username)
+
+
+@lab5.route("/lab5/logout")
+def logout():
+    session.clear()
+    return redirect("/lab5/login")
+
+
+@lab5.route("/lab5/article_publish_list")
+def getPublishList():
+    userID = session.get("id")
+    article_published_list = "Нет опубликованных статей"
+
+    conn = dBConnect()
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT id, title, user_id FROM articles WHERE is_public = true")
+    article_published_list = cur.fetchall()
+
+    return render_template("article_published_list.html", article_published_list=article_published_list)
